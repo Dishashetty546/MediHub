f0ff61d49079865a6f749985411c60fce84d5e5c
dishashetty546 <dishashetty546@gmail.com>
2025-02-28 16:46:12 +0530

    step 3: initialize react app, download all frontend dependencies

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Bootstrap is Working!</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
